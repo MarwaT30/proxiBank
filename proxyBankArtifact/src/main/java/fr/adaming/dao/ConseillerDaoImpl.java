1a6158d69f75c45a12f553c89faeee39bc20b8be
inti0301 <inti0301@INTI-0301>
2016-07-13 11:40:25 +0200

    dao Conseiller

--- proxyBankArtifact/src/main/java/fr/adaming/dao/ConseillerDaoImpl.java
+++ proxyBankArtifact/src/main/java/fr/adaming/dao/ConseillerDaoImpl.java
@@ -1,11 +1,62 @@
 package fr.adaming.dao;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.model.Agence;
+import fr.adaming.model.Client;
+import fr.adaming.model.ConseillerClientele;
 
 @Repository
 @Transactional
 public class ConseillerDaoImpl implements IConseillerDao{
+
+	@Autowired //injection d'une sessionFactory
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public List<Client> getAllConseillers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> getConseillersByAgence(Agence agence) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int ajouterConseiller(ConseillerClientele conseiller) {
+		Session session = sessionFactory.openSession();
+		session.save(conseiller);
+		session.close();
+
+		return 1;
+	}
+
+	@Override
+	public int modifierConseiller(ConseillerClientele conseiller) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int supprimerConseiller(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int supprimerConseiller(ConseillerClientele conseiller) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	
 	
