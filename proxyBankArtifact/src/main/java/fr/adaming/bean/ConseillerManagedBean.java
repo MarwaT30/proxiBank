8e5b544b544d74ed537445670c6d28110c93da6b
Joris <inti0304@INTI-0304>
2016-07-13 13:30:28 +0200

    ConseillerManagedBean

---
+++ proxyBankArtifact/src/main/java/fr/adaming/bean/ConseillerManagedBean.java
@@ -0,0 +1,13 @@
+package fr.adaming.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "conseillerMB")
+@SessionScoped
+public class ConseillerManagedBean implements Serializable {
+	private static final long serialVersionUID = 6531335421273622698L;
+
+}
