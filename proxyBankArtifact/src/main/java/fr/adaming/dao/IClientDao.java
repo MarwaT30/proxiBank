fdaf00cd054fac676b849f51fac9a6c471d4660d
Joris <inti0304@INTI-0304>
2016-07-13 10:35:54 +0200

    IClientDao + correction nom proxibank dans pomme.xml

---
+++ proxyBankArtifact/src/main/java/fr/adaming/dao/IClientDao.java
@@ -0,0 +1,55 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import fr.adaming.model.Client;
+import fr.adaming.model.ConseillerClientele;
+
+public interface IClientDao {
+	/**
+	 * 
+	 * @return tous les clients de la base
+	 */
+	public List<Client> getAllClients();
+
+	/**
+	 * 
+	 * @param conseiller
+	 * @return les clients gérés par ce conseiller
+	 */
+	public List<Client> getClientsByConseiller(ConseillerClientele conseiller);
+
+	/**
+	 * ajoute le client dans la base
+	 * 
+	 * @param client
+	 * @return 1 si OK
+	 */
+	public int ajouterClient(Client client);
+
+	/**
+	 * 
+	 * @param client
+	 *            le client modifié
+	 * @return
+	 */
+	public int modifierClient(Client client);
+
+	/**
+	 * supprime par ID
+	 * 
+	 * @param id
+	 *            l'id du client à supprimer
+	 * @return
+	 */
+	public int supprimerClient(int id);
+
+	/**
+	 * supprimer par Client
+	 * 
+	 * @param client
+	 *            le client avec l'ID à suppriemr
+	 * @return
+	 */
+	public int supprimerClient(Client client);
+}
