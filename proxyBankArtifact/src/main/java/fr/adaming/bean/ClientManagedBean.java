a9f6aeb4b63d6b2968a5539c19c17b9cfe684769
Joris <inti0304@INTI-0304>
2016-07-13 18:23:27 +0200

    modif ClientManagedBean

--- proxyBankArtifact/src/main/java/fr/adaming/bean/ClientManagedBean.java
+++ proxyBankArtifact/src/main/java/fr/adaming/bean/ClientManagedBean.java
@@ -15,10 +15,8 @@ import fr.adaming.service.IClientService;
 public class ClientManagedBean implements Serializable {
 	private static final long serialVersionUID = 615713187L;
 
-	private int id;
-	private String nom, prenom, adresse;
-	private int codePostal;
-	private long telephone;
+	private Client client;
+	
 	IClientService clientService = new ClientServiceImpl();
 	
 	private List<Client> listeClients;
@@ -31,11 +29,9 @@ public class ClientManagedBean implements Serializable {
 	 * Pour créer le client à partir du formulaire
 	 */
 	public void ajouter() {
-		System.out.println(this.getNom() + this.getPrenom() + this.getAdresse()
-				+ this.getCodePostal() + this.getTelephone());
+		System.out.println(client.getNom());
 		
-		clientService.ajouterClient(new Client(this.getNom(), this.getPrenom(),
-				this.getAdresse(), this.getCodePostal(), this.getTelephone()));
+		clientService.ajouterClient(client);
 	}
 
 	/**
@@ -43,54 +39,6 @@ public class ClientManagedBean implements Serializable {
 	 */
 	public ClientManagedBean() {
 		super();
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getAdresse() {
-		return adresse;
-	}
-
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-
-	public int getCodePostal() {
-		return codePostal;
-	}
-
-	public void setCodePostal(int codePostal) {
-		this.codePostal = codePostal;
-	}
-
-	public long getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(long telephone) {
-		this.telephone = telephone;
 	}
 
 	public List<Client> getListeClients() {
